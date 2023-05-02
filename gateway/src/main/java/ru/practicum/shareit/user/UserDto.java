d9ea6d35c1155835fc30a335cd7a310483242485
Ilusha92 <Ushakov@ruvents.com>
2023-05-02 13:50:17 +0300

    testagain
    3

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-
+    private Long id;
     @NotNull
     private String name;
     @Email
